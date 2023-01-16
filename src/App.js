f7717d15d79bca017c3cf55751bce53487889a86
kwonangela <kwonangelaa@gmail.com>
2023-01-15 23:36:03 -0500

    Basic things done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState } from "react";
+import Header from "./Header.jsx"
 
 function App() {
+  const [charInfo, setCharInfo] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    getChar()
+  }, []);
+
+  const getChar = async () => {
+    // const rmData = [];
+    // let url= "https://rickandmortyapi.com/api/character?page="
+    // for (let i=1; i<3; i++){
+    //   fetch(`${url}${i}`)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     rmData.push(...data.results)
+    //   })
+    // }
+    // setCharInfo(rmData);
+  
+    fetch(`https://rickandmortyapi.com/api/character?page=1/`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results);
+      setCharInfo(data.results)
+    })
+    }
+
+  function getNext() {
+    setIndex(thisCharIndex => {
+      if ((thisCharIndex) < 19) {
+        return thisCharIndex + 1;
+      } else {
+        return 0;
+      }
+    })
+  }
+  function getPrev() {
+    setIndex(thisCharIndex => {
+      if ((thisCharIndex) > 0) {
+        return thisCharIndex - 1;
+      } else {
+        return 19;
+      }
+    })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {charInfo.length && 
+    <>
+      <Header />
+      <h2>Characters</h2>
+      <div className="slider">
+        <img className="char" src={charInfo[index].image} alt="asd"/>
+        <br></br>
+        <button className="prev" onClick={getPrev}>&#10094; </button>
+        <button className="next" onClick={getNext}>&#10095;</button>
+      
+      <h3 id="char-id-name">#{charInfo[index].id} - Name: {charInfo[index].name}</h3>
+      <h3>Status: {charInfo[index].status}</h3>
+      <h3>Species: {charInfo[index].species}</h3>
+      <h3>Gender: {charInfo[index].gender}</h3>
+      <h3>Origin: {charInfo[index].origin.name}</h3>
+      <h3>Location: {charInfo[index].location.name}</h3>
+      <h3>Appears in: {charInfo[index].episode.length} episodes</h3>
+      </div>
+    </>
+    }
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <>
+            <h1>Rick and Morty API</h1>
+        </>
+    )
+}
+
+export default Header;
